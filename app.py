e171fba111b354643711578ac80f707a02a423f1
ashwoolford <asrafhossain197@gmail.com>
2024-10-12 09:05:26 +0200

    add: weights

--- app.py
+++ app.py
@@ -19,15 +19,143 @@ from tensorflow.keras import Model, layers
 # variables 
 vocab_size = 10000
 max_seq_len = 40
+embed_dim = 256
+num_heads = 4
+ff_dim = 128
 
 # Initialize Flask app
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+class TransformerLayer(layers.Layer):
+    
+    def __init__(self, num_heads: int, dropout_rate: float, embedding_dims: int, ff_dim: int, **kwargs):
+        super(TransformerLayer, self).__init__(**kwargs)
+        
+        # Initialize Parameters
+        self.num_heads = num_heads
+        self.dropout_rate = dropout_rate
+        self.embedding_dims = embedding_dims
+        self.ff_dim = ff_dim
+        
+        # Initialize Layers
+        self.mha = layers.MultiHeadAttention(num_heads=num_heads, key_dim=embedding_dims, dropout=dropout_rate)
+        self.ln1 = layers.LayerNormalization(epsilon=1e-6)
+        
+        self.ffn = keras.Sequential([
+            layers.Dense(ff_dim, activation='relu', kernel_initializer='he_normal'),
+            layers.Dense(embedding_dims)
+        ])
+        self.ln2 = layers.LayerNormalization(epsilon=1e-6)
+    
+    def call(self, inputs):
+        """Forward pass of the Transformer Layer.
+        
+        Args:
+            inputs: Tensor with shape `(batch_size, seq_len, embedding_dims)` representing the input sequence.
+        
+        Returns:
+            Tensor with shape `(batch_size, seq_len, embedding_dims)` representing the output sequence after applying the Transformer Layer.
+        """
+        
+        # Multi-Head Attention
+        attention = self.mha(inputs, inputs, inputs)
+        
+        # Layer Normalization and Residual Connection
+        normalized1 = self.ln1(attention + inputs)
+        
+        # Feedforward Network
+        ffn_out = self.ffn(normalized1)
+        
+        # Layer Normalization and Residual Connection
+        normalized2 = self.ln2(ffn_out + normalized1)
+        
+        return normalized2
+    
+    def get_config(self):
+        """Get the configuration of the Transformer Layer.
+        
+        Returns:
+            Dictionary with the configuration of the layer.
+        """
+        config = super(TransformerLayer, self).get_config()
+        config.update({
+            "num_heads": self.num_heads,
+            "dropout_rate": self.dropout_rate,
+            "embedding_dims": self.embedding_dims,
+            "ff_dim": self.ff_dim
+        })
+        return config
+
+class TokenAndPositionalEmbedding(layers.Layer):
+    
+    def __init__(self, embedding_dims, vocab_size, seq_len, **kwargs):
+        super(TokenAndPositionalEmbedding, self).__init__(**kwargs)
+        
+        # Initialize parameters
+        self.seq_len = seq_len
+        self.vocab_size = vocab_size
+        self.embedding_dims = embedding_dims
+        self.embed_scale = tf.math.sqrt(tf.cast(embedding_dims, tf.float32))
+        
+        # Define layers
+        self.token_embedding = layers.Embedding(
+            input_dim=vocab_size, 
+            output_dim=embedding_dims,
+            name="token_embedding"
+        )
+        
+        self.positional_embedding = layers.Embedding(
+            input_dim=seq_len, 
+            output_dim=embedding_dims,
+            name="positional_embedding"
+        )
+    
+    def call(self, inputs):
+        seq_len = tf.shape(inputs)[1]
+        
+        # Token Embedding
+        token_embedding = self.token_embedding(inputs)
+        token_embedding *= self.embed_scale
+        
+        # Positional Embedding
+        positions = tf.range(start=0, limit=seq_len, delta=1)
+        positional_embedding = self.positional_embedding(positions)
+        
+        # Add Token and Positional Embedding
+        embeddings = token_embedding + positional_embedding
+        
+        return embeddings
+        
+    
+    def get_config(self):
+        config = super(TokenAndPositionalEmbedding, self).get_config()
+        config.update({
+            'embedding_dims': self.embedding_dims,
+            'vocab_size': self.vocab_size,
+            'seq_len': self.seq_len,
+        })
+        return config
+
 # load pre trained model
-model = tf.keras.models.load_model('./trained_model/my_model.tf')
-#model.summary()
+# Model
+InputLayer = layers.Input(shape=(max_seq_len,), name="InputLayer")
+
+# Embedding Layer
+embeddings = TokenAndPositionalEmbedding(embed_dim, vocab_size, max_seq_len, name="EmbeddingLayer")(InputLayer)
+
+# Transformer Layer
+encodings = TransformerLayer(num_heads=num_heads, embedding_dims=embed_dim, ff_dim=ff_dim, dropout_rate=0.1, name="TransformerLayer")(embeddings)
+
+# Classifier
+gap = layers.GlobalAveragePooling1D(name="GlobalAveragePooling")(encodings)
+drop = layers.Dropout(0.5, name="Dropout")(gap)
+OutputLayer = layers.Dense(1, activation='sigmoid', name="OutputLayer")(drop)
+model = keras.Model(InputLayer, OutputLayer, name="TransformerNet")
+model.load_weights('./trained_model/my_model_weights.h5')
+# model = tf.keras.models.load_model('./trained_model/my_model.tf')
+model.summary()
 
 
 # Specify the path to the SPAM text message dataset
